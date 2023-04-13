11b9309fefb2534b43ce99806fa8efc8299ce7c9
tarik <mahimtk2@gmail.com>
2023-04-13 12:58:05 +0600

    project added

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      Container: {
+        padding: '200rem'
+      },
+    },
+  },
+  plugins: [],
+};
